43f12eca912bcf043e3b21cc04372e9cd66dc67e
ArcticGizmo <jon.howell314@gmail.com>
2023-08-12 21:04:46 +0800

    ability to get camera

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,8 +3,24 @@
     <v-responsive class="align-center text-center fill-height">
       <h2>Welcome</h2>
       <a target="_blank" href="https://pictogrammers.com/library/mdi/">MDI Icons</a>
+      <video ref="videoRef" style="width: 300px; height: 300px; border: 1px solid orange" />
+      <v-btn @click="camera.stop()">Pause</v-btn>
+      <v-btn @click="camera.start()">Play</v-btn>
+      {{ camera.isErrored }} -- {{ camera.permissionDenied }}
     </v-responsive>
   </v-container>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useCamera } from '@/composables/camera';
+import { onMounted } from 'vue';
+import { ref } from 'vue';
+
+const videoRef = ref<HTMLVideoElement>();
+
+const camera = useCamera();
+
+onMounted(() => {
+  camera.bindTo(videoRef.value!);
+});
+</script>
